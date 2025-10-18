ad86a0afd395e6774b4b680c566ccc10e66e4050
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2025-10-18 20:49:16 +0300

    Making Common data class for common variables and Methods Making generic repository for repository pattern Implement Unit Of Work pattern

---
+++ DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Database;
+using NuGet.Protocol.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+       
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+  
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+
+    }
+}
